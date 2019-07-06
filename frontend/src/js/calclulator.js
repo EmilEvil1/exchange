a1b7a16d3035a856d6bf844c8b17acdc6ef737e9
dssorokin <d.sorokin@onvoya.com>
2019-07-06 03:22:28 +0300

    control show logic & calculation logic & dynamic document fields

--- frontend/src/js/calclulator.js
+++ frontend/src/js/calclulator.js
@@ -9,154 +9,230 @@
         // currencies = response;
     });
 
+    function bindEvents() {
+        $(".card_number").blur(function() {
+            validateCreditCard.call(this);
+        });
+
+        $(".crypto_address").blur(function() {
+            validateCryptoAddress.call(this);
+        });
+
+        $(".card_number, .crypto_address").focus(function() {
+            $(this).removeClass("field-error");
+        })
+    }
+
+    function changeConfirmationBlock() {
+        var tickerFrom = $(".calculator__btn--from").data('ticker');
+        var tickerTo = $(".calculator__btn--to").data('ticker');
+        var rateFrom = currencies.find(cur => cur.ticker === tickerFrom).rub;
+        var rateTo = currencies.find(cur => cur.ticker === tickerTo).rub;
+        var rate = rateFrom/rateTo;
+
+        $(".confirmation-block__from-name").text(tickerFrom);
+        $(".confirmation-block__rate").text(rate.toFixed(6));
+        $(".confirmation-block__to-name").text(tickerTo);
+    }
+
+    function changeClientInfoBlock() {
+        var tickerFrom = $(".calculator__btn--from").data('ticker');
+        var tickerTo = $(".calculator__btn--to").data('ticker');
+        var currencyFrom = currencies.find(cur => cur.ticker === tickerFrom);
+        var currencyTo = currencies.find(cur => cur.ticker === tickerTo);
+        var cardNumberTemplate = $("#card_number_template").html();
+        var cryptoAddressTemplate = $("#crypto_address_template").html();
+
+        var compiledFrom = _.template(currencyFrom.paymentDocument === 'card' ? cardNumberTemplate : cryptoAddressTemplate);
+        var compiledTo = _.template(currencyTo.paymentDocument === 'card' ? cardNumberTemplate : cryptoAddressTemplate);
+
+        $(".currency-exchange__input").first().html(compiledFrom(currencyFrom));
+        $(".currency-exchange__input:nth-child(3)").html(compiledTo(currencyTo));
+
+        bindEvents()
+    }
+
     function getResponse(response) {
         currencies = [
             {
                 "ticker": "BTC",
                 "name": "Bitcoin",
                 "rub": 504603.0,
-                "uah": 206124.0
+                "uah": 206124.0,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "ETH",
                 "name": "Ethereum",
                 "rub": 15975.4,
-                "uah": 6525.75
+                "uah": 6525.75,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "XRP",
                 "name": "ripple",
                 "rub": 25.3247,
-                "uah": 10.3449
+                "uah": 10.3449,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "USDT",
                 "name": "Tether",
                 "rub": 64.7203,
-                "uah": 26.4375
+                "uah": 26.4375,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "LTC",
                 "name": "Litecoin",
                 "rub": 5816.45,
-                "uah": 2375.95
+                "uah": 2375.95,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "BCH",
                 "name": "Bitcoin Cash",
                 "rub": 25976.9,
-                "uah": 10611.3
+                "uah": 10611.3,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "EOS",
                 "name": "EOS",
                 "rub": 401.787,
-                "uah": 164.125
+                "uah": 164.125,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "BNB",
                 "name": "Binance Coin",
                 "rub": 1852.71,
-                "uah": 756.812
+                "uah": 756.812,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "TRX",
                 "name": "TRON",
                 "rub": 1.83651,
-                "uah": 0.750195
+                "uah": 0.750195,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "ADA",
                 "name": "Cardano",
                 "rub": 5.45627,
-                "uah": 2.22882
+                "uah": 2.22882,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "XLM",
                 "name": "Stellar",
                 "rub": 8.57784,
-                "uah": 3.50395
+                "uah": 3.50395,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "XMR",
                 "name": "Monero",
                 "rub": 5564.69,
-                "uah": 2273.11
+                "uah": 2273.11,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "DASH",
                 "name": "Dash",
                 "rub": 10801.1,
-                "uah": 4412.12
+                "uah": 4412.12,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "ETC",
                 "name": "Ethereum Classic",
                 "rub": 475.511,
-                "uah": 194.241
+                "uah": 194.241,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "ZEC",
                 "name": "Zcash",
                 "rub": 4952.85,
-                "uah": 2023.18
+                "uah": 2023.18,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "BAT",
                 "name": "Basic Attention Token",
                 "rub": 23.3624,
-                "uah": 9.54328
+                "uah": 9.54328,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "WAVES",
                 "name": "Waves",
                 "rub": 153.838,
-                "uah": 62.841
+                "uah": 62.841,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "USDC",
                 "name": "USD Coin",
                 "rub": 64.65,
-                "uah": 26.4088
+                "uah": 26.4088,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "PAX",
                 "name": "Paxos Standard Token",
                 "rub": 64.4239,
-                "uah": 26.3164
+                "uah": 26.3164,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "HT",
                 "name": "Huobi Token",
                 "rub": 174.635,
-                "uah": 71.3365
+                "uah": 71.3365,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "KCS",
                 "name": "KuCoin Shares",
                 "rub": 66.658,
-                "uah": 27.229
+                "uah": 27.229,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "ZRX",
                 "name": "0x",
                 "rub": 20.4915,
-                "uah": 8.37052
+                "uah": 8.37052,
+                "paymentDocument": "address"
             },
             {
                 "ticker": "ENJ",
                 "name": "Enjin Coin",
                 "rub": 9.68095,
-                "uah": 3.95456
+                "uah": 3.95456,
+                "paymentDocument": "address"
+            },
+            {
+                "ticker": "RUB",
+                "name": "Sberbank",
+                "rub": 1,
+                "paymentDocument": "card"
             }
         ];
-        currencies.forEach(function(currency) {
+        currencies.forEach(function(currency, index) {
             var templateCurrency = $("<div class='currencyItem' data-name='" + currency.name + "' data-ticker='" + currency.ticker + "' data-rub-rate='" + currency.rub + "'>" +
                 "<div style='background-image: url(./svg/" + currency.ticker.toLowerCase() + ".svg)' class=\"selector-currency__icon\"></div>" +
                 "<div class=\"selector-currency__name\">"+ currency.name + "</div>" +
                 "</div>'");
 
-            $(".calculator__selector").append(templateCurrency);
+            if (index < 12) {
+                $(".calculator__selector .selector__row-1").append(templateCurrency);
+            } else {
+                $(".calculator__selector .selector__row-2").append(templateCurrency);
+            }
         });
 
         $(".calculator__btn--to, .calculator__btn--from").click(function(e) {
@@ -167,15 +243,28 @@
             var $calcBtn = $(this).closest('.direction').find(".calculator__btn");
             var name = $(this).data('name');
             var ticker = $(this).data('ticker');
+            var showName = name.length > 13 ? name.slice(0, 13) + '...' : name;
 
-            $calcBtn.html("<span class='payment-method__payment-name'>" + name + "</span>");
-            $calcBtn.attr('data-ticker', ticker);
+            $calcBtn.find('.payment-method__payment-name').text(showName);
+            $calcBtn.find('.payment-method__icon-bank').css('background-image', 'url(./svg/' + ticker.toLowerCase() + '.svg)');
+            $calcBtn.data('ticker', ticker);
+
+            changeConfirmationBlock();
+
+            changeClientInfoBlock();
 
             $(this).closest('.direction').find('.calculator__selector').toggle();
         });
 
         $(".calculator__input--from, .calculator__input--to").keyup(function(e) {
             var currentAmount = $(this).val();
+            var $inputTo = $('.calculator__input--to');
+            var $inputFrom = $('.calculator__input--from');
+            if (!currentAmount) {
+                $inputTo.val('');
+                $inputFrom.val('');
+                return;
+            }
 
             var tickerFrom = $('.calculator__btn--from').data('ticker');
             var tickerTo = $('.calculator__btn--to').data('ticker');
@@ -184,9 +273,9 @@
             var rateTo = currencies.find(cur => cur.ticker === tickerTo).rub;
 
             if ($(this).hasClass('calculator__input--from')) {
-                $('.calculator__input--to').val(rateFrom/rateTo * currentAmount)
+                $inputTo.val((rateFrom/rateTo * currentAmount).toFixed(6))
             } else {
-                $('.calculator__input--from').val(rateTo/rateFrom * currentAmount)
+                $inputFrom.val((rateTo/rateFrom * currentAmount).toFixed(6))
             }
         });
     }
@@ -207,10 +296,11 @@
     });
 
     function validateCreditCard() {
-        var result = $("#card_number").validateCreditCard();
+        var result = $(".card_number").validateCreditCard();
 
         if (!result.valid) {
             $(this).addClass("field-error");
+            $(this).parent().find('.error-msg').show();
             return false;
         }
 
@@ -223,6 +313,7 @@
         var valid = WAValidator.validate(address, ticker);
         if (!valid) {
             $(this).addClass("field-error");
+            $(this).parent().find('.error-msg').show();
             return false;
         }
 
@@ -239,18 +330,6 @@
 
         return true;
     }
-
-    $("#card_number").blur(function() {
-        validateCreditCard();
-    });
-
-    $("#card_number, #crypto_address").focus(function() {
-        $(this).removeClass("field-error");
-    })
-
-    $("#crypto_address").blur(function() {
-        validateCryptoAddress();
-    });
 
     $("#btn-exchange").click(function(e) {
         e.preventDefault();
