7d33402a8896c556df088ee93b9c1d785b42f4e9
dssorokin <d.sorokin@onvoya.com>
2019-09-28 20:58:51 +0300

    remove old front package
    (cherry picked from commit e8f73026d9d2a11c1d005dde3b3a44401392aeba)

--- frontend-temp/src/js/sliderParametrs.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('#slickVertical').slick({
-        vertical: true,
-        verticalSwiping: true,
-        slidesToShow: 1,
-        arrows : false,
-        autoplay: true,
-    });
-});
